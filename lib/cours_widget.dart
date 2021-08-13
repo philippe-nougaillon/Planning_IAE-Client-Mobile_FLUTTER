0b85f03b8dac0789a7a7f3f2bfdc9b4df2246f62
Philippe Nougaillon <philippe.nougaillon@gmail.com>
2021-08-13 07:23:44 +0200

    Logique pour afficher la page 'Details'

--- lib/cours_widget.dart
+++ lib/cours_widget.dart
@@ -15,62 +15,82 @@ class CoursWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Card(
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              flex: 1,
-              child: Container(
-                height: 80,
-                color: HexColor.fromHex(item["formation_color_json_v2"]),
-              ),
-            ),
-            Expanded(
-              flex: 60,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Text(item["debut_fin_json_v2"].substring(0, 5),
-                            style:
-                                const TextStyle(fontWeight: FontWeight.bold)),
-                        Text(item["debut_fin_json_v2"].substring(5)),
-                        Expanded(
-                          child: Align(
-                              alignment: Alignment.centerRight,
-                              child: Text(
-                                item["salle_json_v2"],
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  foreground: Paint()
-                                    ..style = PaintingStyle.stroke
-                                    ..strokeWidth = 1
-                                    ..color = Colors.green,
-                                ),
-                              )),
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 8.0),
-                      child: Text(item["formation_json_v2"],
-                          style: const TextStyle(fontWeight: FontWeight.bold)),
-                    ),
-                    Text(item["intervenant_json"]),
-                    Text(_matiereJson,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.w300,
-                            fontStyle: FontStyle.italic)),
-                  ],
+      child: InkWell(
+        child: Card(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Container(
+                  height: 80,
+                  color: HexColor.fromHex(item["formation_color_json_v2"]),
                 ),
               ),
-            ),
-          ],
+              Expanded(
+                flex: 60,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text(item["debut_fin_json_v2"].substring(0, 5),
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold)),
+                          Text(item["debut_fin_json_v2"].substring(5)),
+                          Expanded(
+                            child: Align(
+                                alignment: Alignment.centerRight,
+                                child: Text(
+                                  item["salle_json_v2"],
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    foreground: Paint()
+                                      ..style = PaintingStyle.stroke
+                                      ..strokeWidth = 1
+                                      ..color = Colors.green,
+                                  ),
+                                )),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: Text(item["formation_json_v2"],
+                            style:
+                                const TextStyle(fontWeight: FontWeight.bold)),
+                      ),
+                      Text(item["intervenant_json"]),
+                      Text(_matiereJson,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.w300,
+                              fontStyle: FontStyle.italic)),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
+        onLongPress: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (BuildContext context) => DetailsPage(item)));
+        },
       ),
     );
+  }
+}
+
+class DetailsPage extends StatelessWidget {
+  DetailsPage(this.item);
+
+  final Map<String, dynamic> item;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(item["formation_json_v2"]);
   }
 }
