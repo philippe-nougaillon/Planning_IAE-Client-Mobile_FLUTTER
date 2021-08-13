b5bd220b07190b2b8b7e26c55d3ff80cb38397f6
Pierre-emmanuel Dacquet <pierre-emmanueldacquet@MacBook-Air-de-Pierre-emmanuel.local>
2021-08-13 14:34:37 +0200

    OnLongPress v1

--- lib/details_page.dart
+++ lib/details_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'hexcolor.dart';
 
 class DetailsPage extends StatelessWidget {
   const DetailsPage(this.item, {Key? key}) : super(key: key);
@@ -7,23 +10,134 @@ class DetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _matiereJson = item["matiere_json"] ?? '';
     return Scaffold(
       appBar: AppBar(
         title: Text(item["formation_json_v2"]),
       ),
-      body: Container(
-        decoration: new BoxDecoration(color: Colors.white),
-        height: 240,
-        child: Stack(
-          children: <Widget>[
-            Image.asset(nomDuPlanAvecSalle(item["salle_json_v2"]),
-                fit: BoxFit.fill),
-            Positioned(
-              bottom: 15,
-              right: 15, //give the values according to your requirement
-              child: Icon(Icons.favorite),
-            ),
-          ],
+      body: Center(
+        child: Card(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 8.0),
+                child: Table(
+                  border: TableBorder.all(
+                    color: HexColor.fromHex(item["formation_color_json_v2"]),
+                  ),
+                  columnWidths: {0: FractionColumnWidth(.3)},
+                  children: [
+                    TableRow(children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("Début",
+                            style:
+                                const TextStyle(fontWeight: FontWeight.bold)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(item["debut_fin_json_v2"].substring(0, 5),
+                            style:
+                                const TextStyle(fontWeight: FontWeight.bold)),
+                      ),
+                    ]),
+                    TableRow(children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("Fin",
+                            style:
+                                const TextStyle(fontWeight: FontWeight.bold)),
+                      ),
+                      Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(item["debut_fin_json_v2"].substring(8))),
+                    ]),
+                    TableRow(children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("Salle",
+                            style:
+                                const TextStyle(fontWeight: FontWeight.bold)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          item["salle_json_v2"],
+                          style: TextStyle(
+                            fontSize: 16,
+                            foreground: Paint()
+                              ..style = PaintingStyle.stroke
+                              ..strokeWidth = 1
+                              ..color = Colors.green,
+                          ),
+                        ),
+                      )
+                    ]),
+                    TableRow(children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("Formation",
+                            style:
+                                const TextStyle(fontWeight: FontWeight.bold)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(item["formation_json_v2"],
+                            style:
+                                const TextStyle(fontWeight: FontWeight.bold)),
+                      )
+                    ]),
+                    TableRow(children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("Intervenant",
+                            style:
+                                const TextStyle(fontWeight: FontWeight.bold)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(item["intervenant_json"]),
+                      )
+                    ]),
+                    TableRow(children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("Cours",
+                            style:
+                                const TextStyle(fontWeight: FontWeight.bold)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(_matiereJson,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.w300,
+                                fontStyle: FontStyle.italic)),
+                      )
+                    ]),
+                    TableRow(children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("Nbr Places",
+                            style:
+                                const TextStyle(fontWeight: FontWeight.bold)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("un certain nombre"),
+                      )
+                    ]),
+                  ],
+                ),
+              ),
+              Text("Emplacement sur la carte",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25)),
+              Container(
+                decoration: new BoxDecoration(color: Colors.white),
+                child: Image.asset(nomDuPlanAvecSalle(item["salle_json_v2"]),
+                    fit: BoxFit.fill),
+              ),
+            ],
+          ),
         ),
       ),
     );
