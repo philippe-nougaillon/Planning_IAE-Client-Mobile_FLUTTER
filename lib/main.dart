8753696988d375e426a28fa57c7bdbee0596626d
Philippe Nougaillon <philippe.nougaillon@gmail.com>
2021-08-19 15:07:38 +0200

    appBar title réduite

--- lib/main.dart
+++ lib/main.dart
@@ -73,7 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: Text('Cours >= ' +
+          title: Text('>= ' +
               formatCurrentDateToFrench(_currentDate) +
               ' ' +
               (_searchText != "" ? '($_searchText)' : '')),
